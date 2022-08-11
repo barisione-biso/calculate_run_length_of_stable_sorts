091036313a81ea5c8a9fd77103e5c801ce99b666
Fabrizio Barisione <fabrizio.barisione@gmail.com>
2022-08-11 11:10:37 -0400

    refactoring

---
+++ build.sh
@@ -0,0 +1,2 @@
+g++ process_run_length.cpp -O3 -o process_run_length -std=c++17 -lboost_serialization
+g++ run_length_check.cpp -O3 -o run_length_check -std=c++17 -lboost_serialization
--- src/build.sh
+++
@@ -1,2 +0,0 @@
-g++ process_run_length.cpp -g -o process_run_length -std=c++17 -lboost_serialization
-g++ run_length_check.cpp -g -o run_length_check -std=c++17 -lboost_serialization
